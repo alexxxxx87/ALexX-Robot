f8b6649bd2f286b113b538a6e507acb2f2f1cd51
alexxxxx87 <122649839+alexxxxx87@users.noreply.github.com>
2023-02-01 05:59:22 +0700

    Add files via upload

---
+++ Niskala/modules/tools.py
@@ -0,0 +1,30 @@
+__mod_name__ = "Tᴏᴏʟꜱ"
+
+__help__ = """
+*AL Info bot*
+✗  /sg <reply> - `To check history name`
+
+*Date-time-Weather*
+✗  /time - `<country code> Gives information about a timezone.`
+✗  /weather - `<city> Get weather info in a particular place.`
+✗  /wttr - `<city> Advanced weather module, usage same as /weather`
+✗  /wttr - `moon Get the current status of moon`
+
+*Converts*
+✗  /encrypt - `Encrypts The Given Text`
+✗  /decrypt - `Decrypts Previously Ecrypted Text`
+✗  /zip - `reply to a telegram file to compress it in .zip format`
+✗  /unzip - `reply to a telegram file to decompress it from the .zip format`
+
+*Random API*
+✗  /ptl *or* /asupan - `get random video from Instagram timeline`
+✗  /chika - `get random video from chikakiku`
+✗  /wibu - `get random short anime video or photos`
+✗  /truth - `challenge`
+✗  /dare - `challenge`
+
+*Tagger*
+✗  /tagall - `for tag everyone at least 100 member first.`
+
+*Maintained By : @IDnyaAL!*
+"""
